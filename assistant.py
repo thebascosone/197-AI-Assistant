e151ffef193c282f37c13b9927f53bf218d250d0
thebascosone <114146047+thebascosone@users.noreply.github.com>
2022-12-08 01:51:53 +0800

    added ASR for testing

--- assistant.py
+++ assistant.py
@@ -1,17 +1,59 @@
-#This would be the main file na magi-initialize ng 4 models and magko-call ng VAD
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import write
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+#from datasets import load_dataset
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+def ASR_Transcribe(voice_input):
+    # load model and tokenizer
+    checkpoint = "openai/whisper-tiny.en"
+    processor = WhisperProcessor.from_pretrained(checkpoint)
+    model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
+
+    # tokenize
+    input_features = processor(voice_input, return_tensors="pt", sampling_rate=16000).input_features
+
+    # retrieve logits
+    #logits = model(input_features, decoder_input_ids = torch.tensor([[50258]])).logits
+
+    # take argmax and decode
+    #predicted_ids = torch.argmax(logits, dim=-1)
+    generated_ids = model.generate(inputs=input_features, max_new_tokens=20)
+    return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
 def GenerateReply(human_input):
-  checkpoint = "facebook/blenderbot-400M-distill"
-  tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-  model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+    #load model and tokenizer
+    model_name = "microsoft/DialoGPT-medium"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
-  model_inputs = tokenizer(human_input, padding=True, return_tensors='pt')
-  model_replies = model.generate(**model_inputs)
-  return tokenizer.batch_decode(model_replies, skip_special_tokens=True)[0]
+    #tokenize input and generate output
+    user_input_ids = tokenizer.encode(human_input + tokenizer.eos_token, return_tensors='pt')
+    #bot_input_ids = torch.cat([chat_history_ids, user_input_ids], dim=-1) if step > 0 else user_input_ids
+    bot_input_ids = user_input_ids
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-while (1):
-  txt_input = input("Human: ") #should be a string coming from the ASR block
-  reply = GenerateReply([txt_input])
-  print("Bot: ", reply) #instead of print, pass output to STT block
+    #decode batch ids into human language
+    return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status, flush=True)
+
+freq = 16000
+duration = 5
+
+#while (1):
+#stream = sd.InputStream(samplerate=freq, dtype='float32', channels=1, callback=callback)
+stream = sd.rec(int(duration*freq), samplerate=freq, channels=1)
+print("Human: ")
+#stream.start()
+sd.wait()
+sd.play(stream, freq)
+
+txt_input = ASR_Transcribe(np.transpose(stream)[0])
+print(txt_input)
+refly = GenerateReply(txt_input)
+print("ChatBot: ", refly)
